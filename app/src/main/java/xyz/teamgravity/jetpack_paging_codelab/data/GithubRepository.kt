acea7c5c514c46d3d1369c3dd66f034b79ed4390
raheemadamboev <raheemideadev@gmail.com>
2021-12-14 23:49:46 +0500

    setup github paging source and github repository

---
+++ app/src/main/java/xyz/teamgravity/jetpack_paging_codelab/data/GithubRepository.kt
@@ -0,0 +1,31 @@
+package xyz.teamgravity.jetpack_paging_codelab.data
+
+import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import xyz.teamgravity.jetpack_paging_codelab.api.GithubService
+import xyz.teamgravity.jetpack_paging_codelab.model.RepositoryModel
+
+class GithubRepository(private val service: GithubService) {
+
+    /**
+     * Search repositories whose names match the query, exposed as a stream of data that will emit
+     * every time we get more data from the network.
+     */
+    fun getSearchResultStream(query: String): Flow<PagingData<RepositoryModel>> {
+        Log.d("GithubRepository", "New query: $query")
+        return Pager(
+            config = PagingConfig(
+                pageSize = NETWORK_PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { GithubPagingSource(service, query) }
+        ).flow
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 30
+    }
+}
