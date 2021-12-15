4b0ceb64c3933583f337b0b7224735cf6b2f4979
raheemadamboev <raheemideadev@gmail.com>
2021-12-15 21:32:56 +0500

    setup resources and layout designs

--- app/src/main/java/xyz/teamgravity/jetpack_paging_codelab/ui/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/jetpack_paging_codelab/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package xyz.teamgravity.jetpack_paging_codelab
+package xyz.teamgravity.jetpack_paging_codelab.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.teamgravity.jetpack_paging_codelab.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
